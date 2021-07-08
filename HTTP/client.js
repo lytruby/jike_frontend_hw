087ed6451f5d46bf351850b4b2f47bc656b0e8c3
LiuYitian <lytruby51@gmail.com>
2021-07-08 12:20:50 +0800

    submission

---
+++ HTTP/client.js
@@ -0,0 +1,195 @@
+const net = require("net");
+
+//?这是在构造什么，一个类吗
+class Request {
+    constructor(options) {
+        this.method = options.method || "GET";
+        this.host = options.host;
+        this.port = options.port || 80;
+        this.path = options.path || "/";
+        this.body = options.headers || {};
+        //一定要有content type
+        if (!this.headers["Content-Type"]) {
+            this.headers["Content-Type"] = "application/x-www-form-urlencoded";
+        }
+        if (this.headers["Content-Type"] === "application/json")
+            this.bodyText = JSON.stringify(this.body);
+        else if (this.headers["Content-Type"] === "application/x-www-form-urlencoded")
+            this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&');
+        this.headers["Content-Length"] = this.bodyText.length;
+    }
+    //根据request格式
+    toString() {
+        return `${this.method} ${this.path} HTTP/1.1 \r
+        ${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r
+        \r
+        ${this.bodyText}`
+    }
+    //在一个建立好的tcp连接上把请求发送出去
+    //若没有则根据host和port创建tcp连接
+    send(connection) {
+        //send是一个promise的形式，在send的过程中会逐步收到response，在response构造好之后再让promise得到resolve
+        //parser通过逐步的接收response信息去构造response对象
+        //创建ResponseParser这个类
+        return new Promise((resolve, reject) => {
+            const parser = new ResponseParser;
+            if (connection) {
+                connection.write(this.toString());
+            } else {
+                connection = net.createConnection({
+                    host: this.host,
+                    port: this.port
+                }, () => {
+                    connection.write(this.toString());
+                })
+            }
+            connection.on('data', (data) => {
+                parser.receive(data.toString());
+                if (parser.isFinished) {
+                    resolve(parser.response);
+                }
+                connection.end();
+            });
+            connection.on('error', (err) => {
+                reject(err);
+                connection.end();
+            });
+        });
+    }
+}
+
+class ResponseParser {
+    constructor() {
+        this.WAITING_STATUS_LINE = 0;
+        this.WAITING_STATUS_LINE_END = 1;
+        this.WAITING_HEADER_NAME = 2;
+        this.WAITING_HEADER_SPACE = 3;
+        this.WAITING_HEADER_VALUE = 4;
+        this.WAITING_HEADER_LINE_END = 5;
+        this.WAITING_HEADER_BLOCK_END = 6;
+        this.WAITING_BODY = 7;
+
+        this.current = this.WAITING_STATUS_LINE;
+        this.statusLine = "";
+        this.headers = {};
+        this.headerName = "";
+        this.headerValue = "";
+        this.bodyParser = null;
+    }
+    receive(string) {
+        for (let i = 0; i < string.length; i++) {
+            //这是状态方程
+            this.receiveChar(string.charAt(i));
+        }
+    }
+    receiveChar(char) {
+        if (this.current === this.WAITING_STATUS_LINE) {
+            if (char === '\r') {
+                this.current = this.WAITING_STATUS_LINE_END;
+            } else {
+                this.statusLine += char;
+            }
+        } else if (this.current === this.WAITING_STATUS_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME;
+            }
+        } else if (this.current === this.WAITING_HEADER_NAME) {
+            if (char === ':') {
+                this.current = this.WAITING_HEADER_SPACE;
+            } else if (char === '\r') {
+                this.current = this.WAITING_HEADER_BLOCK_END;
+                //bodyParser的方法是根据headers里面的transfer-encoding来决定的，并不是一开始就定义好的
+                if (this.headers['Transfer-Encoding'] === 'chunked')
+                    this.bodyParser = new TrunkedBodyParser();
+            } else {
+                this.headerName += char;
+            }
+        } else if (this.current === this.WAITING_HEADER_SPACE) {
+            if (char === '') {
+                this.current = this.WAITING_HEADER_VALUE;
+            }
+        } else if (this.current === this.WAITING_HEADER_VALUE) {
+            if (char === '\r') {
+                this.current = this.WAITING_HEADER_LINE_END;
+                this.headers[this.headerName] = this.headerValue;
+                this.headerName = "";
+                this.headerValue = "";
+            } else {
+                this.headerValue += char;
+            }
+        } else if (this.current === this.WAITING_HEADER_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME;
+            }
+        } else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_BODY
+            }
+        } else if (this.current === this.WAITING_BODY) {
+            this.bodyParser.receiveChar(char);
+        }
+    }
+}
+class TrunkedBodyParser {
+    constructor() {
+        this.WAITING_LENGTH = 0;
+        this.WAITING_LENGTH_LINE_END = 1;
+        this.READING_TRUNK = 2;
+        this.WAITING_NEW_LINE = 3;
+        this.WAITING_NEW_LINE_END = 4;
+        this.length = 0;
+        this.content = [];
+        this.isFinished = false;
+        this.current = this.WAITING_LENGTH;
+    }
+    receiveChar(char) {
+        if (this.current = this.WAITING_LENGTH) {
+            if (char === '\r') {
+                if (this.length === 0) {
+                    this.isFinished = true;
+                }
+                this.current = this.WAITING_LENGTH_LINE_END;
+            } else {
+                //把之前的十六进制数进一位，再加上当前的数
+                this.length *= 16;
+                this.length += parseInt(char, 16);
+            }
+        } else if (this.current === this.WAITING_NEW_LINE_END) {
+            if (char === '\n') {
+                this.current = this.READING_TRUNK;
+            }
+        } else if (this.current === this.READING_TRUNK) {
+            this.content.push(char);
+            this.length--;
+            if (this.length === 0) {
+                this.current = this.WAITING_NEW_LINE;
+            }
+        } else if (this.current === this.WAITING_NEW_LINE) {
+            if (char === '\r') {
+                this.current = this.WAITING_NEW_LINE_END;
+            }
+        } else if (this.current === this.WAITING_NEW_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_LENGTH;
+            }
+        }
+    }
+}
+
+
+void async function () {
+    let request = new Request({
+        method: "POST",
+        host: "127.0.0.1",
+        port: "8088",
+        path: "/",
+        headers: {
+            ["X-Foo2"]: "customed"
+        },
+        body: {
+            name: "winter"
+        }
+    });
+    let response = await request.send();
+    console.log(response);
+}();
